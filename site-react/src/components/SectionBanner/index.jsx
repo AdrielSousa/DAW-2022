e26576536b4672bb58adeed0cb8c474a7de8b285
AdrielSousa <110207314+AdrielSousa@users.noreply.github.com>
2022-12-04 14:29:55 -0300

    AULA 20/10/2022
    Estou comitando agora pois nesta data meu notebook havia quebrado e não tinha máquina para continuar as alterações no site.

--- site-react/src/components/SectionBanner/index.jsx
+++ site-react/src/components/SectionBanner/index.jsx
@@ -1,46 +1,6 @@
-import { useState } from 'react';
-import { useImovel } from '../../hook/useImovel';
 import './style.css';
 
-const operacoes = [
-    { valor: '', rotulo: 'Operação' },
-    { valor: 'venda', rotulo: 'Venda' },
-    { valor: 'locacao', rotulo: 'Locação' }
-];
-
-const tipos = [
-    { valor: '', rotulo: 'Tipo Imóvel' },
-    { valor: 'apto', rotulo: 'Apartamento' },
-    { valor: 'casa', rotulo: 'Casa' },
-    { valor: 'sobrado', rotulo: 'Sobrado' },
-    { valor: 'terreno', rotulo: 'Terreno' }
-];
-
-const cidades = [
-    { valor: '', rotulo: 'Cidade' },
-    { valor: 'coronel', rotulo: 'Coronel Vivida' },
-    { valor: 'beltrao', rotulo: 'Francisco Beltrão' },
-    { valor: 'pato', rotulo: 'Pato Branco' },
-    { valor: 'saudade', rotulo: 'Saudade do Iguaçu' }
-]
-
-const bairros = [
-    { valor: '', rotulo: 'Bairro' },
-    { valor: 'brasilia', rotulo: 'Brasilia' },
-    { valor: 'centro', rotulo: 'Centro' },
-    { valor: 'lasalle', rotulo: 'La Salle' },
-    { valor: 'pinheiro', rotulo: 'Pinheiros' }
-];
-
 export function SectionBanner() {
-
-    const { imoveis, setParamFilter } = useImovel();
-
-    const [filter, setFilter] = useState({});
-
-    function handleFilter() {
-        setParamFilter(filter)
-    }
 
     return (
         <section id="banner">
@@ -50,43 +10,44 @@ export function SectionBanner() {
             </div>
             <div className="banner-filter">
                 <div className="filter-fields">
-                    
+
                     <form>
-                        <select
-                            onChange={event => setFilter({...filter, operacao: event.target.value})}>
-                            {operacoes.map((item,index) => (
-                                <option key={ index } value={ item.valor }>{ item.rotulo }</option>
-                            ))}
+                        <select>
+                            <option value="venda">Venda</option>
+                            <option value="locacao">Locação</option>
                         </select>
 
-                        <select
-                            onChange={event => setFilter({...filter, tipo: event.target.value})}>
-                            {tipos.map((item,index) => (
-                                <option key={ index } value={ item.valor }>{ item.rotulo }</option>
-                            ))}
+                        <select>
+                            <option>Tipo Imóvel</option>
+                            <option value="apto">Apartamento</option>
+                            <option value="casa">Casa</option>
+                            <option value="sobrado">Sobrado</option>
+                            <option value="terreno">Terreno</option>
                         </select>
 
-                        <select
-                            onChange={event => setFilter({...filter, cidade: event.target.value})}>
-                            {cidades.map((item,index) => (
-                                <option key={ index } value={ item.valor }>{ item.rotulo }</option>
-                            ))}                            
+                        <select>
+                            <option>Cidade</option>
+                            <option value="coronel">Coronel Vivida</option>
+                            <option value="beltrao">Francisco Beltrão</option>
+                            <option value="pato">Pato Branco</option>
+                            <option value="saudade">Saudade do Iguaçu</option>
                         </select>
 
-                        <select
-                            onChange={event => setFilter({...filter, bairro: event.target.value})}>
-                            {bairros.map((item,index) => (
-                                <option key={ index } value={ item.valor }>{ item.rotulo }</option>
-                            ))}                           
+                        <select>
+                            <option>Bairro</option>
+                            <option value="brasilia">Brasilia</option>
+                            <option value="centro">Centro</option>
+                            <option value="lasalle">La Salle</option>
+                            <option value="pinheiro">Pinheiros</option>
                         </select>
                     </form>
 
                 </div>
 
                 <div className="filter-result">
-                    <p className="result-value">{ imoveis.length }</p>
-                    <p className="result-label">{ imoveis.length != 1 ? 'imóveis' : 'imóvel' }</p>
-                    <button onClick={ handleFilter }>Filtrar</button>
+                    <p className="result-value">26</p>
+                    <p className="result-label">Imóveis</p>
+                    <button>Filtrar</button>
                 </div>
             </div>
         </section>
--- site-react/src/components/SectionCards/index.jsx
+++ site-react/src/components/SectionCards/index.jsx
@@ -1,46 +1,86 @@
-import { IntlProvider, FormattedNumber } from 'react-intl';
-import { useImovel } from '../../hook/useImovel';
-
 import './style.css';
 
 
 export function SectionCards() {
-
-    const { imoveis } = useImovel();
-
     return (
         <section id="cards">
             <h2>Imóveis disponíveis</h2>
 
-            {imoveis.length > 0 ? (
-                <div className="cards-content">
+            <div className="cards-content">
 
-                    {imoveis.map((item, index) => (
-                        <div key={index} className="card">
-                            <img src={item.foto} alt="Casa 1" />
-                            <div className="card-content">
-                                <h4>{item.titulo}</h4>
-                                <p>{item.descricao}</p>
+                <div className="card">
+                    <img src="./assets/img/thumb1.png" alt="Casa 1" />
+                    <div className="card-content">
+                        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                        <p>Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque
+                            voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.</p>
 
-                                <div className="card-price">
-                                    <p>
-                                        <IntlProvider locale="pt-BR">
-                                            <FormattedNumber value={item.preco}
-                                                style="currency"
-                                                currency="BRL" />
-                                        </IntlProvider>
-                                    </p>
-                                </div>
-                            </div>
+                        <div className="card-price">
+                            <p>R$ 900.000,00</p>
                         </div>
-                    ))}
-
+                    </div>
                 </div>
-            ) : (
-                <h3 className="not-found">Nenhum imóvel foi encontrado!</h3>
-            ) }
 
+                <div className="card">
+                    <img src="./assets/img/thumb2.png" alt="Casa 2" />
+                    <div className="card-content">
+                        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                        <p>Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque
+                            voluptates eligendi recusandae consequuntur non ullam fugiat.</p>
+                        <div className="card-price">
+                            <p>R$ 900.000,00</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div className="card">
+                    <img src="./assets/img/thumb3.png" alt="Casa 3" />
+                    <div className="card-content">
+                        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                        <p>Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.</p>
+                        <div className="card-price">
+                            <p>R$ 900.000,00</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src="./assets/img/thumb4.png" alt="Casa 4" />
+                    <div className="card-content">
+                        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                        <p>Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque
+                            voluptates eligendi recusandae consequuntur non ullam fugiat.</p>
+                        <div className="card-price">
+                            <p>R$ 900.000,00</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src="./assets/img/thumb5.png" alt="Casa 5" />
+                    <div className="card-content">
+                        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                        <p>Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque
+                            voluptates eligendi recusandae consequuntur non ullam fugiat.</p>
+                        <div className="card-price">
+                            <p>R$ 900.000,00</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src="./assets/img/thumb6.png" alt="Casa 6" />
+                    <div className="card-content">
+                        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h4>
+                        <p>Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque
+                            voluptates eligendi recusandae consequuntur non ullam fugiat.</p>
+                        <div className="card-price">
+                            <p>R$ 900.000,00</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </section>
     )
 }
